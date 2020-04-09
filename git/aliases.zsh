f769e7c82b86783c6db7350bdf28df6171f151c2
Vicken Simonian <vsimon@gmail.com>
2020-04-08 23:08:26 -0700

    Git defaults

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,15 +1,12 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
-
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias gf='git fetch --all --prune'
+alias grom='git rebase origin/master'
+alias gprune='git branch --merged master | grep -v 'master$' | xargs git branch -d'
+alias gsgr='git stash && git fetch && git rebase origin/master && git stash pop'
+alias gcan='git commit -a --amend --no-edit'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
