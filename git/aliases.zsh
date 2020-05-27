8507bfb2d4af38de6efa74ada92df0ae31953d7a
Vicken Simonian <vsimon@gmail.com>
2020-05-26 21:08:16 -0700

    brew, alias updates

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,6 +1,5 @@
 # The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gl='git log'
 alias gp='git push origin HEAD'
 alias gf='git fetch --all --prune'
 alias grom='git rebase origin/master'
@@ -9,14 +8,12 @@ alias gprune='git branch --merged master | grep -v 'master$' | xargs git branch 
 alias gsgr='git stash && git fetch && git rebase origin/master && git stash pop'
 alias gcan='git commit -a --amend --no-edit'
 
-# Remove `+` and `-` from start of diff lines; just rely upon color.
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
-
+alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gs='git status'
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
