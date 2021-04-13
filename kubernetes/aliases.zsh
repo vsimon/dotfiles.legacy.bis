dd8ef33ed0838f0953024cc3c77e2cbe7ecca206
Vicken Simonian <vsimon@gmail.com>
2021-04-13 12:42:35 -0700

    Aliases

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -2,3 +2,7 @@ autoload -U +X compinit && compinit
 source <(kubectl completion zsh)
 alias k=kubectl
 complete -F __start_kubectl k
+
+if [[ -a aliases.local.zsh ]]; then
+  source aliases.local.zsh
+fi
