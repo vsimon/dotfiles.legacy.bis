982d3f827e584e9a6c072f929a77e54da11a9aef
Vicken Simonian <vsimon@gmail.com>
2021-07-08 19:31:19 -0700

    Aliases, tmux

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -2,6 +2,7 @@ autoload -U +X compinit && compinit
 source <(kubectl completion zsh)
 alias k=kubectl
 complete -F __start_kubectl k
+alias t=telepresence
 
 if [[ -a aliases.local.zsh ]]; then
   source aliases.local.zsh
